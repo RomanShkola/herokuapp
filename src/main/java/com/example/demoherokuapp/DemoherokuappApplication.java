3277cb1fca4b25f446f0fd6b1157fb65b94ae990
Roman Shkola <roman.shkola@dhl.com>
2021-10-13 18:21:46 +0200

    first Heroku app

---
+++ src/main/java/com/example/demoherokuapp/DemoherokuappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoherokuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoherokuappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoherokuappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoherokuapp/DemoherokuappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoherokuapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoherokuappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
